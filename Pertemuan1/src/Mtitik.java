780b1e8b616b8cec237fba92e449977e32896ff0
hekalrapli <haikalrfli11@gmail.com>
2026-02-19 17:15:55 +0700

    Hapus Folder Intro

--- Pertemuan1/src/Mtitik.java
+++ Pertemuan1/src/Mtitik.java
@@ -1,3 +1,10 @@
+/* Nama file  : Titik.java
+   Deskripsi  :berisi atribut dan method dalam class Titik
+   pembuat : Haikal Rafli Sembiring/24060124130079
+   tanggal : 19 Februari 2026
+ */
+
+
 public class Mtitik {
     public static void main(String[] args) {
         Titik T1 = new Titik();
--- Pertemuan1/src/Titik.java
+++ Pertemuan1/src/Titik.java
@@ -1,4 +1,8 @@
-
+/* Nama file  : Titik.java
+   Deskripsi  :berisi atribut dan method dalam class Titik
+   pembuat : Haikal Rafli Sembiring/24060124130079
+   tanggal : 19 Februari 2026
+ */
 
 
 public class Titik {
