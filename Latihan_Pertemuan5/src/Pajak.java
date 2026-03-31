3ef1582c1d745c1e751119ca4e3cc7ec2a61c8c8
hekalrapli <haikalrfli11@gmail.com>
2026-03-31 08:24:05 +0700

    Latihan Pertemuan 5

---
+++ Latihan_Pertemuan5/src/Pajak.java
@@ -0,0 +1,10 @@
+/* Nama file  : Pajak.java
+   Deskripsi  :berisi method dalam interface pajak
+   pembuat : Haikal Rafli Sembiring/24060124130079
+   tanggal : 31 Maret 2026
+ */
+
+public interface Pajak {
+    /*Method */
+    public double hitungPajak();
+}
